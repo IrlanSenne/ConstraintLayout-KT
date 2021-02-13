b4578ff8ee8bf37fa2e5f96128a5fed2635c1bc6
Moreira <im.senne2017@gmail.com>
2021-02-13 12:06:06 +0000

    update

--- app/src/main/java/com/senne/constraint/Coffe.kt
+++ app/src/main/java/com/senne/constraint/Coffe.kt
@@ -7,5 +7,9 @@ class Coffe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffe)
+
+        if(supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
     }
 }
