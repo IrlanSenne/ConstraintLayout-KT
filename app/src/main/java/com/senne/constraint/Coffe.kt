ef900bc25f8be94f7c804360fffd9f906ae547ab
Moreira <im.senne2017@gmail.com>
2021-02-13 11:38:31 +0000

    Coffe Screen OK - Layout

---
+++ app/src/main/java/com/senne/constraint/Coffe.kt
@@ -0,0 +1,11 @@
+package com.senne.constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Coffe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffe)
+    }
+}
